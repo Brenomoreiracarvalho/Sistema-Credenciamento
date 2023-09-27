0a3ecf7213427362f25eea82c9f01d271bd414fa
Breno Moreira <breno.moreira@hotmail.com>
2023-09-27 10:12:52 -0400

    botao submit na pagina de login

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <q-input v-model="email" filled type="email" hint="Login" />
-    <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="Password">
+    <q-input
+      v-model="password"
+      filled
+      :type="isPwd ? 'password' : 'text'"
+      hint="Password"
+    >
       <template v-slot:append>
         <q-icon
           :name="isPwd ? 'visibility_off' : 'visibility'"
@@ -10,21 +15,41 @@
         />
       </template>
     </q-input>
+    <q-btn
+      type="submit"
+      :loading="submitting"
+      label="Save"
+      class="q-mt-md"
+      color="teal"
+      @click="submit()"
+    >
+      <template v-slot:loading>
+        <q-spinner-facebook />
+      </template>
+    </q-btn>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { ref } from 'vue'
+import { defineComponent } from "vue";
+import { ref } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage',
-  setup () {
+  name: "IndexPage",
+  setup() {
+    const submitting = ref(false);
+    function submit() {
+      submitting.value = true;
+      setTimeout(() => {
+        submitting.value = false;
+      }, 3000);
+    }
     return {
       isPwd: ref(true),
-
-      email: ref('')
-    }
-  }
-})
+      email: ref(""),
+      submitting,
+      submit,
+    };
+  },
+});
 </script>
--- src/pages/singIn/Index.vue
+++ src/pages/singIn/Index.vue
@@ -1,32 +1,48 @@
 <template>
-    <div>
-      <h1>SIngIn</h1>
-      <q-input v-model="email" filled type="email" hint="Login" />
-      <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="Password">
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-      </q-input>
-    </div>
-  </template>
-  
-  <script>
-  import { defineComponent } from 'vue'
-  import { ref } from 'vue'
-  
-  export default defineComponent({
-    name: 'SingIn',
-    setup () {
+  <div>
+    <h1>SIngIn</h1>
+    <q-input v-model="email" filled type="email" hint="Login" />
+    <q-input
+      v-model="password"
+      filled
+      :type="isPwd ? 'password' : 'text'"
+      hint="Password"
+    >
+      <template v-slot:append>
+        <q-icon
+          :name="isPwd ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @click="isPwd = !isPwd"
+        />
+      </template>
+    </q-input>
+
+    <q-btn type="submit" color="primary" id="pdf" @click="submit()" no-caps
+      ><b>{{ $t("buttons.download_pdf") }}</b></q-btn
+    >
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { ref } from "vue";
+
+export default defineComponent({
+  name: "SingIn",
+  setup() {
+    const submitting = ref(false);
+    function submit() {
+      submitting.value = true;
+      setTimeout(() => {
+        submitting.value = false;
+      }, 3000);
+    }
     return {
       isPwd: ref(true),
-
-      email: ref('')
-    }
-  }
-  })
-  </script>
-  
+      email: ref(""),
+      submitting,
+      submit,
+    };
+  },
+});
+</script>
