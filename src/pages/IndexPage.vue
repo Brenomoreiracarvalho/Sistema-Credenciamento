2dba0f834333133abb762cd2ba418fbe9110f767
Breno Moreira <breno.moreira@hotmail.com>
2023-10-07 20:09:46 -0300

    Centralização dos campos de login e senha

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,32 +1,45 @@
 <template>
-  <div>
-    <q-input v-model="email" filled type="email" hint="Login" />
-    <q-input
-      v-model="password"
-      filled
-      :type="isPwd ? 'password' : 'text'"
-      hint="Password"
-    >
-      <template v-slot:append>
-        <q-icon
-          :name="isPwd ? 'visibility_off' : 'visibility'"
-          class="cursor-pointer"
-          @click="isPwd = !isPwd"
-        />
-      </template>
-    </q-input>
-    <q-btn
-      type="submit"
-      :loading="submitting"
-      label="Save"
-      class="q-mt-md"
-      color="teal"
-      @click="submit()"
-    >
-      <template v-slot:loading>
-        <q-spinner-facebook />
-      </template>
-    </q-btn>
+  <div class="container">
+    <div class="box">
+      <q-input v-model="email" filled type="email" hint="Login" />
+      <q-input
+        v-model="password"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+        hint="Password"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+
+      <q-btn
+        type="submit"
+        label="ESQUECI A SENHA"
+        class="q-mt-md"
+        @click="forgotPassword()"
+      >
+        <template v-slot:loading>
+          <q-spinner-facebook />
+        </template>
+      </q-btn>
+      <q-btn
+        type="submit"
+        :loading="submitting"
+        label="ENTRAR"
+        class="q-mt-md"
+        color="teal"
+        @click="submit()"
+      >
+        <template v-slot:loading>
+          <q-spinner-facebook />
+        </template>
+      </q-btn>
+    </div>
   </div>
 </template>
 
@@ -39,6 +52,14 @@ export default defineComponent({
   setup() {
     const submitting = ref(false);
     function submit() {
+      submitting.value = true;
+      if (submitting.value === true) {
+      }
+      setTimeout(() => {
+        submitting.value = false;
+      }, 3000);
+    }
+    function forgotPassword() {
       submitting.value = true;
       setTimeout(() => {
         submitting.value = false;
@@ -53,3 +74,14 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
